a8e9dac52d2e88fb49f00b3ce023323797e2aa66
Ade Prianto <adeprianto21@upi.edu>
2020-08-05 14:11:16 +0700

    Menambahkan Seeder Untuk Admin Table

---
+++ src/database/seeders/20200805070706-demo-admin.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('Admins', [
+      {
+        id: 1,
+        name: 'Admin',
+        username: 'admin',
+        password: '123456',
+        email: 'admin@tes.com',
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Admins', null, {});
+  },
+};
