aa00a1ff619bb7f4b93b13627e8f7a48906e388c
Ade Prianto <adeprianto21@upi.edu>
2020-08-05 14:01:57 +0700

    Menambahkan Model & Migrations

--- src/database/migrations/20200805064847-create-category.js
+++ src/database/migrations/20200805064847-create-category.js
@@ -1,27 +1,27 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('tes', {
+    await queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
-      nama: {
-        type: Sequelize.STRING
+      category: {
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('tes');
-  }
+    await queryInterface.dropTable('Categories');
+  },
 };
